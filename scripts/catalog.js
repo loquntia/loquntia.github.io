81873d5f136c89a05057f6bc4e1a268425e8c89f
loquntia <loquntia@gmail.com>
2016-06-23 16:56:36 -0500

    test

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,8 +1,9 @@
 
 var tipuesearch = {"pages": [
  
-{"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"}
-
+{"title": "Home", "text": "Kingdom of Loquntia Home Page", "tags": "Kingdom Loquntia Home", "url": "http://loquntia.github.io/index.html"},
+{"title": "Login", "text": "Restricted Resources Login", "tags": "whitelist restricted resource login user", "url": "http://loquntia.github.io/login.html"},
+{"title": "Converter", "text": "Micronational Currency Converter", "tags": "currency micronational dollar convert calculator", "url": "http://loquntia.github.io/mex/converter.html"},
 ]};
 
 
